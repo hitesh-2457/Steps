1b5c2546753c5e2881669fb7d72ea64e2987ffe5
hitesh <hitesh.2457@outlook.com>
2018-04-26 18:49:58 -0500

    Step Detector working

--- app/src/main/java/com/utd/mxp165130/steps/TabFragment1.java
+++ app/src/main/java/com/utd/mxp165130/steps/TabFragment1.java
@@ -1,12 +1,14 @@
 package com.utd.mxp165130.steps;
 
+import android.content.Context;
+import android.hardware.*;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import com.utd.mxp165130.steps.R;
 
 public  class TabFragment1 extends Fragment {
 
@@ -20,6 +22,7 @@ public  class TabFragment1 extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        return rootView;
     }
 }
