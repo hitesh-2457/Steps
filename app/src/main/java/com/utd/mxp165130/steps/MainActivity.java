4a7928f4e640af9e29a8596e22a77e05737ec27e
hitesh <hitesh.2457@outlook.com>
2018-04-26 19:03:35 -0500

    Fixing merge issue

--- app/src/main/java/com/utd/mxp165130/steps/MainActivity.java
+++ app/src/main/java/com/utd/mxp165130/steps/MainActivity.java
@@ -1,28 +1,31 @@
 package com.utd.mxp165130.steps;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
 import android.support.design.widget.TabLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
-
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import static android.support.constraint.Constraints.TAG;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private static SensorManager mSensorManager;
+    private static Sensor mStepDetector;
+    private int initialCount=0;
+    private TextView counter;
 
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
@@ -53,12 +56,16 @@ public class MainActivity extends AppCompatActivity {
         // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
+        mViewPager.setOffscreenPageLimit(2);
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
 
         mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
         tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
 
+        mSensorManager = (SensorManager)this.getSystemService(SENSOR_SERVICE);
+        mStepDetector = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
+        this.counter = (TextView) findViewById(R.id.textView8);
 
 //        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
 //
@@ -69,7 +76,6 @@ public class MainActivity extends AppCompatActivity {
 //        recyclerView.setAdapter(rAdapter);
 
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -93,40 +99,71 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
+    @Override
+    public void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mStepDetector, SensorManager.SENSOR_DELAY_NORMAL);
     }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        Log.i(TAG, "onSensorChanged: --------------------"+String.valueOf((int)event.values[0]));
+        this.initialCount += (int) event.values[0];
+        counter.setText(String.valueOf(initialCount));
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    public void startRec(View v){
+        counter = ((View)v.getParent()).findViewById(R.id.textView8);
+        counter.setVisibility(View.VISIBLE);
+        this.initialCount = 0;
+        counter.setText("0");
+    }
+
+//    /**
+//     * A placeholder fragment containing a simple view.
+//     */
+//    public static class PlaceholderFragment extends Fragment {
+//        /**
+//         * The fragment argument representing the section number for this
+//         * fragment.
+//         */
+//        private static final String ARG_SECTION_NUMBER = "section_number";
+//
+//        public PlaceholderFragment() {
+//        }
+//
+//        /**
+//         * Returns a new instance of this fragment for the given section
+//         * number.
+//         */
+//        public static PlaceholderFragment newInstance(int sectionNumber) {
+//            PlaceholderFragment fragment = new PlaceholderFragment();
+//            Bundle args = new Bundle();
+//            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+//            fragment.setArguments(args);
+//            return fragment;
+//        }
+//
+//        @Override
+//        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                                 Bundle savedInstanceState) {
+//            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+//            TextView textView = (TextView) rootView.findViewById(R.id.textView8);
+//            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+//            return rootView;
+//        }
+//    }
 
 
 
@@ -140,23 +177,25 @@ public class MainActivity extends AppCompatActivity {
         public SectionsPagerAdapter(FragmentManager fm) {
             super(fm);
         }
+        private static final String TAG = "MyActivity";
 
         @Override
         public Fragment getItem(int position) {
-             if(position == 0){
-                 return TabFragment1.newInstance();
-             }
-             else if(position == 1) {
-                 return  TabFragment2.newInstance();
-             }
-             else  {
-                 return TabFragment3.newInstance();
-             }
+            Log.i(TAG,"getItem: --------------------"+position+"--------------------");
+            if(position == 0){
+                return TabFragment1.newInstance();
             }
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            //return PlaceholderFragment.newInstance(position + 1);
-           // return TabFragment1.newInstance();
+            else if(position == 1) {
+                return TabFragment2.newInstance();
+            }
+            else  {
+                return TabFragment3.newInstance();
+            }
+        }
+        // getItem is called to instantiate the fragment for the given page.
+        // Return a PlaceholderFragment (defined as a static inner class below).
+        //return PlaceholderFragment.newInstance(position + 1);
+        // return TabFragment1.newInstance();
 
         @Override
         public int getCount() {
